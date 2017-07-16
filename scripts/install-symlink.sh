775d9c89fe3bffbf37acf3866118479b1f3bd7e9
hekt <hektorg@gmail.com>
2017-07-16 15:24:52 +0900

    improve creating symlink

--- scripts/install-symlink.sh
+++ scripts/install-symlink.sh
@@ -10,11 +10,7 @@ FILES=(
 )
 
 for f in ${FILES[@]}; do
-    # if symlink to directory already exists
-    # `ln` creates symlink on linked directory
-    if [ -e $HOME/$f ]; then
-        echo "$HOME/$f already exists"
-    else
-        ln -s $PARENT_DIR/files/$f $HOME/$f
+    if [ ! -e $HOME/$f ] || [ -h $HOME/$f ]; then
+        ln -fns $PARENT_DIR/files/$f $HOME/$f
     fi
 done
