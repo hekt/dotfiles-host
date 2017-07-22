d2346eed56aef320bda82148ca0853eed9d5349d
hekt <hektorg@gmail.com>
2017-07-22 20:36:13 +0900

    remove markdown-mode file

--- files/.emacs.d/inits/50_markdown.el
+++
@@ -1,6 +0,0 @@
-;; markdown-mode
-(add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
-
-(defun markdown-mode-hooks ()
-  (setq markdown-command "mdown"))
-(add-hook 'markdown-mode-hook 'markdown-mode-hooks)
