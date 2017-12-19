e1bda386d4dd596c9823fc5dafe25b458338b5a1
hekt <hektorg@gmail.com>
2017-12-20 00:44:38 +0900

    remove unnecessary custom variables

--- files/.emacs.d/.custom-set-variables.el
+++ files/.emacs.d/.custom-set-variables.el
@@ -3,8 +3,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(helm-buffer-max-length 28)
- '(helm-truncate-lines t t)
  '(package-selected-packages
    (quote
     (neotree toml-mode rust-mode yaml-mode init-loader auto-complete helm js2-mode markdown-mode multiple-cursors php-mode popwin typescript-mode yasnippet))))
