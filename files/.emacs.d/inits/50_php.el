0886f83fcd551a9a28b8a07631b4c3e2c0138627
hekt <hektorg@gmail.com>
2017-12-30 16:11:50 +0900

    use-package php-mode

--- files/.emacs.d/inits/50_php.el
+++ files/.emacs.d/inits/50_php.el
@@ -1,14 +1,12 @@
-;; php-mode
-(add-to-list 'auto-mode-alist '("\\.php$" . php-mode))
-
-(defun php-mode-hooks ()
-  (define-key php-mode-map (kbd "(") 'skeleton-pair-insert-maybe)
-  (define-key php-mode-map (kbd "{") 'skeleton-pair-insert-maybe)
-  (setq skeleton-pair 1)
-  (setq tab-width 4
-        c-basic-offset 4
-        indent-tabs-mode t))
-(add-hook 'php-mode-hook 'php-mode-hooks)
-
-;; flycheck
-(add-hook 'php-mode-hook 'flycheck-mode)
+(use-package php-mode
+  :mode
+  (("\\.php$" . php-mode))
+  :bind
+  (:map php-mode-map
+   ("(" . 'skeleton-pair-insert-maybe)
+   ("{" . 'skeleton-pair-insert-maybe))
+  :init
+  (add-hook 'php-mode-hook (lambda () (setq tab-width 4 c-basic-offset 4 indent-tabs-mode t)))
+  ;; flycheck
+  ;; (add-hook 'php-mode-hook 'flycheck-mode)
+  )
