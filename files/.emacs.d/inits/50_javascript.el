a9c1b6dd8c6abd6c7d1d1c59c3df7ef5b3de331f
hekt <hektorg@gmail.com>
2017-12-31 09:19:26 +0900

    use-package js2-mode, typescript-mode

--- files/.emacs.d/inits/50_javascript.el
+++ files/.emacs.d/inits/50_javascript.el
@@ -1,32 +1,30 @@
-;; javascript-mode
-(setq js-indent-level 2)
+(use-package js2-mode
+  :mode
+  (("\\.js$" . js2-mode))
+  :config
+  (setq js2-basic-offset 2)
+  (setq js2-strict-inconsistent-return-warning nil)
+  (setq js2-strict-trailing-comma-warning nil)
+  (setq js2-indent-switch-body t)
+  (setq-default
+   js2-global-externs
+   '("setTimeout" "clearTimeout" "setInterval" "clearInterval"
+     "location" "console" "JSON" "navigator"
+     ;; new apis
+     "FileReader" "localStorage" "sessionStorage" "history"
+     "MutationObserver"
+     ;; libs
+     "$" "jQuery" "Vue" "chrome"
+     ;; node.js
+     "global" "require" "process" "module" "exports"
+     ;; mocha
+     "describe" "it" "before" "after" "beforeEach" "afterEach"
+     ;; Web Extensions
+     "browser"
+     ))
+  )
 
-;; js2-mode
-(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
-(setq js2-basic-offset 2)
-(setq js2-strict-inconsistent-return-warning nil)
-(setq js2-strict-trailing-comma-warning nil)
-(setq js2-indent-switch-body t)
-(setq-default js2-global-externs
-              '("setTimeout" "clearTimeout" "setInterval" "clearInterval"
-                "location" "console" "JSON" "navigator"
-                ;; new apis
-                "FileReader" "localStorage" "sessionStorage" "history"
-                "MutationObserver"
-                ;; libs
-                "$" "jQuery" "Vue" "chrome"
-                ;; node.js
-                "global" "require" "process" "module" "exports"
-                ;; mocha
-                "describe" "it" "before" "after" "beforeEach" "afterEach"
-                ;; Web Extensions
-                "browser"
-                ))
-
-;; typescript-mode
-(defun typescript-mode-hooks ()
-  "typescript-mode-hook"
-  (auto-complete-mode t)
-  (set (make-local-variable 'tab-width) 2)
-  (setq typescript-indent-level 2))
-(add-hook 'typescript-mode-hook 'typescript-mode-hooks)
+(use-package typescript-mode
+  :config
+  (setq typescript-indent-level 2)
+  )
