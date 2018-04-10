7c7757b7b7d1c07196832e7d3e6a7408806093c3
hekt <hektorg@gmail.com>
2018-04-11 00:05:36 +0900

    improve php-mode configs

--- files/.emacs.d/inits/50_php.el
+++ files/.emacs.d/inits/50_php.el
@@ -11,6 +11,12 @@
         (while (re-search-forward "/" nil t)
           (replace-match "\\\\"))))))
 
+(use-package company-php
+  :defer t
+  :config
+  (ac-php-core-eldoc-setup)
+  )
+
 (use-package php-mode
   :mode
   (("\\.php$" . php-mode))
@@ -18,20 +24,19 @@
   (:map php-mode-map
    ("(" . 'skeleton-pair-insert-maybe)
    ("{" . 'skeleton-pair-insert-maybe)
-   ("C-c n" . 'php-ns-replace))
+   ("C-c n" . 'php-ns-replace)
+   ("C-c j" . 'ac-php-find-symbol-at-point)
+   ("C-c b" . 'ac-php-location-stack-back))
   :init
-  (add-hook 'php-mode-hook
-            (lambda ()
-              (setq tab-width 4 c-basic-offset 4 indent-tabs-mode t)
-              ;; ac-php
-              (require 'ac-php)
-              (setq ac-sources '(ac-source-php))
-              (require 'company-php)
-              (company-mode t)
-              (ac-php-core-eldoc-setup) ;; enable eldoc
-              (make-local-variable 'company-backends)
-              (add-to-list 'company-backends 'company-ac-php-backend)
-              ))
+  (defun my/php-mode-hook ()
+    (setq tab-width 4)
+    (setq c-basic-offset 4)
+    (setq indent-tabs-mode t)
+    ;; company-php
+    (make-local-variable 'company-backends)
+    (add-to-list 'company-backends 'company-ac-php-backend)
+    )
+  (add-hook 'php-mode-hook 'my/php-mode-hook)
   ;; flycheck
   ;; (add-hook 'php-mode-hook 'flycheck-mode)
   )
