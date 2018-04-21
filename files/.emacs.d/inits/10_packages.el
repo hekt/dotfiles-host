b2baab15197654075af7ba306cbd5adc23fdbf22
hekt <hektorg@gmail.com>
2018-04-21 23:05:21 +0900

    simplify whgitespace-mode config

--- files/.emacs.d/inits/10_packages.el
+++ files/.emacs.d/inits/10_packages.el
@@ -132,18 +132,7 @@
 (use-package whitespace
   :ensure t
   :config
-  ;; display tab/space
-  ;; http://d.hatena.ne.jp/syohex/20110119/1295450495
-  (setq whitespace-style '(face tabs tab-mark spaces space-mark))
-  (setq whitespace-display-mappings
-        '((space-mark ?\u3000 [?\u25a1])
-          ;; WARNING: the mapping below has a problem.
-          ;; When a TAB occupies exactly one column, it will display the
-          ;; character ?\xBB at that column followed by a TAB which goes to
-          ;; the next TAB column.
-          ;; If this is a problem for you, please, comment the line below.
-          (tab-mark ?\t [?\xBB ?\t] [?\\ ?\t])))
-  (setq whitespace-space-regexp "\\(\u3000+\\)")
+  (setq whitespace-style '(face tabs tab-mark))
   (global-whitespace-mode 1)
   )
 
