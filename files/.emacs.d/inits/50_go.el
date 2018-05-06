e65e2b64f32ffcce7fe2e5f220d6c13c30f0beca
hekt <hektorg@gmail.com>
2018-05-06 20:16:44 +0900

    add go-lang configs

---
+++ files/.emacs.d/inits/50_go.el
@@ -0,0 +1,27 @@
+(use-package go-mode
+  :ensure t
+  :defer t
+  :bind
+  (:map go-mode-map
+        ("C-c i" . 'go-import-add)
+        ("C-c j" . 'godef-jump)
+        ("C-c b" . 'pop-tag-mark))
+  :init
+  (add-hook 'go-mode-hook 'my/go-mode-hook)
+  (add-hook 'go-mode-hook 'go-eldoc-setup)
+  (add-hook 'before-save-hook 'gofmt-before-save)
+  )
+
+(use-package company-go
+  :ensure t
+  :defer t
+  )
+
+(use-package go-eldoc
+  :ensure t
+  :defer t
+  )
+
+(defun my/go-mode-hook ()
+  (make-local-variable 'company-backends)
+  (add-to-list 'company-backends 'company-go))
