e817caf34314e23fcf3c54e050366583f6566d79
hekt <hektorg@gmail.com>
2019-09-16 02:49:07 +0900

    bind C-x [/] to beginning/end-of-buffer

--- files/.emacs.d/inits/10_keybinds.el
+++ files/.emacs.d/inits/10_keybinds.el
@@ -2,6 +2,8 @@
 (global-set-key (kbd "C-c r") 'replace-string)
 (global-set-key (kbd "C-c M-r") 'replace-regexp)
 (global-set-key (kbd "C-m") 'newline-and-indent)
+(global-set-key (kbd "C-x [") 'beginning-of-buffer)
+(global-set-key (kbd "C-x ]") 'end-of-buffer)
 
 ;; bind C-h as delete-backward-char in mini buffer
 (define-key key-translation-map [?\C-h] [?\C-?])
